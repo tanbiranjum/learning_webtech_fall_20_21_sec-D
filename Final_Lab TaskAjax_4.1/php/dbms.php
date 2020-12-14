e51c23421a744f27a73ef44e90f0ffe29e78e9ea
tanbiranjum <tanbiranjum@hotmail.com>
2020-12-14 09:39:02 +0600

    second commit

--- Final_Lab TaskAjax_4.1/php/dbms.php
+++ Final_Lab TaskAjax_4.1/php/dbms.php
@@ -7,12 +7,43 @@ if (isset($_REQUEST['submit'])) {
     $name = $_SERVER['name'];
     $email = $_SERVER['email'];
     $type = $_SERVER['type'];
+
+    registerUser($id, $password, $name, $email, $type, $connection);
 }
 
 
 
 
 
-function registerUser($id, $password, $name, $email, $type) {
-    $sql = 'INSERT INTO '
+function registerUser($id, $password, $name, $email, $type, $connection)
+{
+    $sql = "INSERT INTO users (id, pwd, name, email, type) VALUES($id, $password, $name, $email, $type)";
+    try {
+        $result = mysqli_query($connection, $sql);
+        if ($result) {
+            header('Location: ../view/login.php?status=success');
+        } else {
+            header('Location: ../index.php?status=error');
+        }
+    } catch (Exception $e) {
+        header('Location: ../index.php?status=error');
+    }
+}
+
+function loginUser($id, $password, $connection)
+{
+    $sql = "SELECT id FROM users WHERE id = $id AND pwd = $password";
+
+    try {
+        $result = mysqli_query($connection, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        if ($count == 1) {
+            header("location: ../view/home.php");
+        } else {
+            header("location: ../view/login.php");
+        }
+    } catch (Exception $e) {
+        echo $e;
+    }
 }
---
+++ Final_Lab TaskAjax_4.1/view/forgotpwd.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHANGE Password</title>
+</head>
+
+<body>
+    <form action="">
+        <fieldset>
+            <legend>CHANGE PASSWORD</legend>
+            <label for="currentpwd">Password</label>
+            <input type="password" name="currentpwd" id="pwd1">
+            <label for="newpwd">Password</label>
+            <input type="password" name="newpwd" id="pwd2">
+            <label for="confirmpwd">Password</label>
+            <input type="password" name="confirmpwd" id="pwd3">
+            <hr>
+            <button type="submit">Change</button>
+            <a href="home.html">Home</a>
+        </fieldset>
+    </form>
+</body>
+
+</html>
+
+<style>
+    label {
+        display: block;
+    }
+
+    form {
+        width: 20%;
+        margin: 0 auto;
+    }
+</style>
